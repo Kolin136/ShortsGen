0e6fcc2fc5b25855cf07f2e56684152fce5ec05a
Kolin136 <jsowb136@gmail.com>
2024-12-18 16:04:14 +0900

    "refactor:비디오분리 코드 동적으로 수정"

--- controller/FileController.py
+++ controller/FileController.py
@@ -1,59 +1,36 @@
-from flask import Flask, jsonify, Config
+from flask import Flask, jsonify,Blueprint,request,current_app
 import os,json
 
 from service.FileService import FileService
 
-app = Flask(__name__)
-app.config.from_object(Config)  # 설정 파일 적용
-output_dir = "../static/segments"
-os.makedirs(output_dir, exist_ok=True)
-segment_duration = 300 #분할 영상 길이 단위
+# Blueprint 정의
+fileController = Blueprint('FileController', __name__)
+
+segmentSaveDir = "./static/video/segments"
+os.makedirs(segmentSaveDir, exist_ok=True) #디렉토리 생성(이미 존재하면 패스)
+segment_duration = 10 #분할 영상 길이 단위
 
 fileService = FileService()
 
-@app.route('/test_split')
-def test_split():
-  video_path = r"C:\Users\jsowb\Desktop\gate.mp4"
+@fileController.route('/test_split',methods=['POST'])
+def videoSplit():
+  videoFile = request.files['video']
+  filename = videoFile.filename  # 원본 파일명
+  save_path = os.path.join( "./static/video/original", filename)  # 저장 경로
 
+  # 파일 저장
+  videoFile.save(save_path)
 
-  segments = fileService.fileSplit(video_path, output_dir, segment_duration)
+  segments = fileService.videoSplit(save_path, filename, segmentSaveDir, segment_duration)
 
   response = {
     "message": "Video split successfully!",
     "segments": segments
   }
-  # JSON 데이터를 보기 좋게 포매팅
-  formatted_response = json.dumps(response, ensure_ascii=False, indent=4)
+  # # JSON 데이터를 보기 좋게 포매팅
+  # formatted_response = json.dumps(response, ensure_ascii=False, indent=4)
 
-  return app.response_class(
-      response=formatted_response,
-      status=200,
-      mimetype="application/json"
-  )
+  return jsonify(response)
 
-if __name__ == '__main__':
-  app.run(debug=True)
-
-
-# 영상 업로드 및 분리 엔드포인트
-# @app.route('/upload', methods=['POST'])
-# def upload_video():
-#   # 사용자가 업로드한 파일 받아오기
-#   video_file = request.files['video']
-#   segment_duration = int(request.form.get('segment_duration', 60))  # 원하는 분리 길이 (초)
-#
-#   # 저장 경로 설정
-#   upload_path = f"./uploads/{video_file.filename}"
-#   output_dir = "./segments"
-#   os.makedirs(output_dir, exist_ok=True)  # 분리된 영상 저장 디렉토리 생성
-#
-#   # 파일 저장
-#   video_file.save(upload_path)
-#
-#   # 영상 분리 및 저장
-#   segments = split_video_with_intervals(upload_path, output_dir, segment_duration)
-#
-#   # 반환
-#   return jsonify({"message": "Video split successfully!", "segments": segments})
 
 
