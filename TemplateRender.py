768a5af2883d9656c85f02be4ac5bc398fea1eae
Kolin136 <jsowb136@gmail.com>
2025-01-31 13:27:25 +0900

    feat:비디오분리+프롬프트CRUD 화면 API 코드 추가

---
+++ TemplateRender.py
@@ -0,0 +1,13 @@
+from flask_restx import Namespace, Resource
+from flask import request,url_for, send_file, Response, stream_with_context,render_template,Flask,make_response
+
+# 네임스페이스
+templateNamespace = Namespace('5.TemplateRender',description='TemplateRender api 목록')
+
+
+@templateNamespace.route('/index')
+class index(Resource):
+  def get(self):
+    response = make_response(render_template("index.html"))
+    response.headers["Content-Type"] = "text/html"
+    return response
